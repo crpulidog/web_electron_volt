373acc37cd2928162448cc0311307c39b4d4d34a
Carolina Pulido <crpulidog1521@gmail.com>
2026-03-08 18:43:37 -0500

    Agrgar el esqueleto del navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import { Header } from './assets/components/Header'
 
 function App() {
 //  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>CodingTUbes</h1>
-    </>
+    <main className='border border-White w-[375px] m-auto'>
+      <section className='bg-Navy-850 p-6 h-[420px]'>
+        <Header />
+      </section>
+    </main>
   )
 }
 
